4d94d8e0fd2ac738f8b8fbe6b872913efdd52879
myrdall <trivarson@gmail.com>
2020-10-16 12:17:20 +0700

    add tests

--- test/test.cpp
+++ test/test.cpp
@@ -1,3 +1,23 @@
+#include "../thirdparty/catch.hpp"
+#include "../functions.h"
 
+#define CATCH_CONFIG_MAIN
+
+
+TEST_CASE("check first perscent function"){
+
+        int n = 1000;
+
+        int percent = 10;
+
+        int percent_amount = n * percent / 100;
+
+        int income = n - percent_amount;
+        
+        REQUIRE(percent_amount == 100);
+        REQUIRE(income == 900);
+
+        } 
+        
 
 
